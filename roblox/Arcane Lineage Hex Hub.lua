c9fd76f968b7a8d2e6917e238378af5bd18b3314
Credux <77338896+Credux@users.noreply.github.com>
2023-08-30 15:31:08 +0800

    Update Arcane Lineage Hex Hub.lua

--- roblox/Arcane Lineage Hex Hub.lua
+++ roblox/Arcane Lineage Hex Hub.lua
@@ -649,7 +649,7 @@ Teleports:AddDropdown({
         elseif Value == "Inferno Parkour Attempt1" then
 -- Correct path: decrease CFrame.new(-this, N/A, -this
 -- This too: CFrame.new(Front -Reverse, N/A, Left -Right
-            local CFrameEnd = CFrame.new(-3944.75781, 53.6970673, -2783.07324, -0.0124968914, -5.6133743e-08, 0.999921918,
+            local CFrameEnd = CFrame.new(-3744.75781, 53.6970673, -2783.07324, -0.0124968914, -5.6133743e-08, 0.999921918,
                 -6.00345373e-08, 1, 5.53878223e-08, -0.999921918, -5.93376726e-08, -0.0124968914)
             local Time = 0
             local tween = game:GetService("TweenService"):Create(lp.Character.HumanoidRootPart,
@@ -657,7 +657,7 @@ Teleports:AddDropdown({
             tween:Play()
                         elseif Value == "Inferno Parkour Attempt2" then
 -- Correct path: decrease CFrame.new(-this, N/A, -this
-            local CFrameEnd = CFrame.new(-3944.75781, 53.6970673, -2683.07324, -0.0124968914, -5.6133743e-08, 0.999921918,
+            local CFrameEnd = CFrame.new(-3844.75781, 53.6970673, -2683.07324, -0.0124968914, -5.6133743e-08, 0.999921918,
                 -6.00345373e-08, 1, 5.53878223e-08, -0.999921918, -5.93376726e-08, -0.0124968914)
             local Time = 0
             local tween = game:GetService("TweenService"):Create(lp.Character.HumanoidRootPart,
@@ -675,7 +675,7 @@ Teleports:AddDropdown({
                         elseif Value == "Inferno Parkour Attempt4" then
 -- Correct path: decrease CFrame.new(-this, N/A, -this
 -- This too: CFrame.new(Front -Reverse, N/A, Left -Right
-            local CFrameEnd = CFrame.new(-3944.75781, 53.6970673, -2483.07324, -0.0124968914, -5.6133743e-08, 0.999921918,
+            local CFrameEnd = CFrame.new(-3644.75781, 53.6970673, -2483.07324, -0.0124968914, -5.6133743e-08, 0.999921918,
                 -6.00345373e-08, 1, 5.53878223e-08, -0.999921918, -5.93376726e-08, -0.0124968914)
             local Time = 0
             local tween = game:GetService("TweenService"):Create(lp.Character.HumanoidRootPart,

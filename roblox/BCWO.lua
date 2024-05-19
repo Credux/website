b78587fdbc814d4c65f675ac3b055d44861fc58e
Credux <77338896+Credux@users.noreply.github.com>
2024-05-19 18:51:11 +0800

    Update BCWO.lua

--- roblox/BCWO.lua
+++ roblox/BCWO.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Credux/Main/Script/roblox/BCWO%20Men.lua'))()
  
 local Window = Rayfield:CreateWindow({
    Name = "BCW:O Script",
